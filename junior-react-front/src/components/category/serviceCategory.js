867d6ee9405d300a0313cb0f8355a13b61639c37
OlgaMenkoMouse <90971820+OlgaMenkoMouse@users.noreply.github.com>
2022-03-20 16:28:06 +0300

    Creating a header for the Category component.

--- junior-react-front/src/components/category/serviceCategory.js
+++ junior-react-front/src/components/category/serviceCategory.js
@@ -2,23 +2,32 @@
 export const Category = () => {
     return (
         <div className="category">
-            <div className="ulGender">
-                <ul className="gender">
-                    <li className="women">
-                        Women
-                    </li>
-                    <li className="men">
-                        Men
-                    </li>
-                    <li className="kids">
-                        Kids
-                    </li>
-                </ul>
-            </div>
-            <div className="green">
-                <img src="/img/green.png" alt="green" />
-                <img src="/img/arow.png" alt="arow" />
-            </div>
+            <header>
+                <div className="ulGender">
+                    <ul className="gender">
+                        <li className="sex">
+                            WOMEN
+                        </li>
+                        <li className="sex">
+                            MEN
+                        </li>
+                        <li className="sex">
+                            KIDS
+                        </li>
+                    </ul>
+                </div>
+                <div className="green">
+                    <img className="greeN" src="/img/green.png" alt="green" />
+                    <img className="arow" src="/img/arow.png" alt="arow" />
+                    <img className="arow2" src="/img/arow2.png" alt="arow2" />
+                </div>
+                <div className="cartOnRight">
+                    <img className="dollar" src="/img/$.png" alt="$" />
+                    <img className="vector" src="/img/Vector.png" alt="vector" />
+                    <img className="cart" src="/img/Icon/Vector.png" alt="cart" />
+                </div>
+            </header>
+
 
         </div>
     );
