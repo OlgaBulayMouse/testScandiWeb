963dc1858fd2459289d21a0f19f2bcf45e66a9de
OlgaMenkoMouse <90971820+OlgaMenkoMouse@users.noreply.github.com>
2022-03-20 22:27:04 +0300

    Create product items html, css

--- junior-react-front/src/components/category/serviceCategory.js
+++ junior-react-front/src/components/category/serviceCategory.js
@@ -2,33 +2,94 @@
 export const Category = () => {
     return (
         <div className="category">
-            <header>
-                <div className="ulGender">
-                    <ul className="gender">
-                        <li className="sex">
-                            WOMEN
-                        </li>
-                        <li className="sex">
-                            MEN
-                        </li>
-                        <li className="sex">
-                            KIDS
-                        </li>
-                    </ul>
-                </div>
-                <div className="green">
-                    <img className="greeN" src="/img/green.png" alt="green" />
-                    <img className="arow" src="/img/arow.png" alt="arow" />
-                    <img className="arow2" src="/img/arow2.png" alt="arow2" />
-                </div>
-                <div className="cartOnRight">
-                    <img className="dollar" src="/img/$.png" alt="$" />
-                    <img className="vector" src="/img/Vector.png" alt="vector" />
-                    <img className="cart" src="/img/Icon/Vector.png" alt="cart" />
-                </div>
-            </header>
+            <body>
+                <header>
+                    <div className="ulGender">
+                        <ul className="gender">
+                            <li className="sex">
+                                WOMEN
+                            </li>
+                            <li className="sex">
+                                MEN
+                            </li>
+                            <li className="sex">
+                                KIDS
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="green">
+                        <img className="greeN" src="/img/green.png" alt="green" />
+                        <img className="arow" src="/img/arow.png" alt="arow" />
+                        <img className="arow2" src="/img/arow2.png" alt="arow2" />
+                    </div>
+                    <div className="cartOnRight">
+                        <img className="dollar" src="/img/$.png" alt="$" />
+                        <img className="vector" src="/img/Vector.png" alt="vector" />
+                        <img className="cart" src="/img/Icon/Vector.png" alt="cart" />
+                    </div>
+                </header>
+                <main>
+                    <div className="title">
+                        Category name
+                    </div>
+                    <div className="container">
+                        <div className="product">
+                            <img className="product1" src="/img/Product card/Elements/Product D.png" alt="product1">
+                            </img>
+                            <div className="inscription">
+                                <div className="name">Apollo Running Short</div>
+                                <div className="price">$50.00</div>
+                            </div>
+                        </div>
 
+                        <div className="product">
+                            <img className="product2" src="/img/Product card/Elements/Product B.png" alt="product2">
+                            </img>
+                            <div className="inscription">
+                                <div className="name">Apollo Running Shor</div>
+                                <div className="price">$50.00</div>
+                            </div>
+                        </div>
 
+                        <div className="product">
+                            <img className="product3" src="/img/Product card/Elements/Rectangle 2135.png" alt="product3">
+                            </img>
+                            <div className="inscription">
+                                <div className="name">Apollo Running Short</div>
+                                <div className="price">$50.00</div>
+                            </div>
+                        </div>
+
+                        <div className="product">
+                            <img className="product4" src="/img/Product card/Elements/Product A.png" alt="product4">
+                            </img>
+                            <div className="inscription">
+                                <div className="name">Apollo Running Short</div>
+                                <div className="price">$50.00</div>
+                            </div>
+                        </div>
+
+                        <div className="product">
+                            <img className="product5" src="/img/Product card/Elements/Product D.png" alt="product5">
+                            </img>
+                            <div className="inscription">
+                                <div className="name">Apollo Running Short</div>
+                                <div className="price">$50.00</div>
+                            </div>
+                        </div>
+
+                        <div className="product">
+                            <img className="product6" src="/img/Product card/Elements/Product A.png" alt="product6">
+                            </img>
+                            <div className="inscription">
+                                <div className="name">Apollo Running Short</div>
+                                <div className="price">$50.00</div>
+                            </div>
+                        </div>
+
+                    </div>
+                </main>
+            </body>
         </div>
     );
 };
